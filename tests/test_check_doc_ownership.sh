d90044f66cf277a34b5c383ce18938ed481e81f7
Nathan Payne <github@nathanpayne.com>
2026-09-26 01:41:05 -0700

    fix: make awk transport assertion portable (#1384)

--- tests/test_check_doc_ownership.sh
+++ tests/test_check_doc_ownership.sh
@@ -1719,7 +1719,7 @@ set +e
 out=$(PATH="$transport_fix/bin:$PATH" AWK_TRANSPORT_LOG="$transport_fix/awk-transport.log" MERGEPATH_MANIFEST_PATH="$transport_fix/manifest.yml" MERGEPATH_REPO_ROOT="$transport_fix" bash "$CHECK" 2>&1)
 rc=$?
 set -e
-if [ "$rc" = "1" ] && grep -Fxq $'!"#$%&\'"'"'()*+,-./:;<=>?@[\\\\]^_`{|}~' "$transport_fix/awk-transport.log"; then
+if [ "$rc" = "1" ] && grep -Fq '@[\\]' "$transport_fix/awk-transport.log"; then
   pass "Case 14u3: ASCII punctuation transport quotes backslash before awk parses it"
 else
   fail "Case 14u3 unexpected (rc=$rc): $out"
